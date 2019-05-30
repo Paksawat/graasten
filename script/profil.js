df2499f0d69c0bcd9efdbab0ff2a2ce413f79b2d
CLS114 <32448434+CLS114@users.noreply.github.com>
2019-05-30 12:09:22 +0200

    Javascript
    Alt javascript til profil.
    lavet hover effeter på billeder og tekst.

---
+++ script/profil.js
@@ -0,0 +1,35 @@
+// variable med alle profilbilleder
+var picture = document.querySelectorAll(".ProfilPicture");
+
+// Loop igennem alle billeder
+for (var i = 0; i < picture.length; i++){
+  // mouseover event på det billede med mus på
+  picture[i].addEventListener("mouseover", function(){
+    // hover billedet og skalere den 
+    this.style.transform = "scale(1.2)";
+    // Finder hover billedets forrige og næste søskende node
+    var nextSibling = this.nextSibling;
+    var prevSibling = this.previousSibling;
+    //ændre teksten opacity
+    nextSibling.nextSibling.style.opacity = "1";
+    prevSibling.previousSibling.style.opacity = "1";
+    //ændre hvor langt fra bunden de skal sidde
+    nextSibling.nextSibling.style.bottom = "-10%";
+    prevSibling.previousSibling.style.bottom = "85%";
+  });
+  // mouseout event på det billede med mus på
+  picture[i].addEventListener("mouseout", function(){
+    // hover billedet og skalere den 
+    this.style.transform = "scale(1)";
+    
+    // Finder hover billedets forrige og næste søskende node
+    var nextSibling = this.nextSibling;
+    var prevSibling = this.previousSibling;
+    //ændre teksten opacity
+    nextSibling.nextSibling.style.opacity = "0";
+    prevSibling.previousSibling.style.opacity = "0";
+    //ændre hvor langt fra bunden de skal sidde
+    nextSibling.nextSibling.style.bottom = "50%";
+    prevSibling.previousSibling.style.bottom = "50%";
+  });
+}
